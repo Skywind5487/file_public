db585707d87ae891c4480c5673caa4ed7eb647f3
Skywind5487 <skywind5487@gmail.com>
2024-06-02 14:09:43 +0800

    'NewVersion'

--- backup_to_baidu.bat
+++ backup_to_baidu.bat
@@ -8,7 +8,7 @@ set source=C:\Users\weiti\Desktop\PARA\1_project\MC\file_public
 set destination=Z:\baidu\file_public
 
 :: 建立一個應該被排除的文件列表
-set excludeFiles="C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\.gitattributes" "C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\.gitignore" "C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\README.md"
+set excludeFiles="C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\.gitattributes" "C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\.gitignore" "C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\README.md" "C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\backup_to_baidu.bat"
 
 :: 建立一個應該被排除的目錄列表
 set excludeDirs="C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\.git"
