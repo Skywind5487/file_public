0c74cc16118f2c2c5b192f669130e1401ca4945f
Skywind5487 <skywind5487@gmail.com>
2024-06-02 14:09:04 +0800

    'NewVersion'

--- backup_to_baidu.bat
+++ backup_to_baidu.bat
@@ -4,13 +4,16 @@ git add .
 git commit -m'NewVersion'
 git push -f
 
-
 set source=C:\Users\weiti\Desktop\PARA\1_project\MC\file_public
 set destination=Z:\baidu\file_public
 
-robocopy "%source%" "%destination%" /MIR /mt /z
+:: 建立一個應該被排除的文件列表
+set excludeFiles="C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\.gitattributes" "C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\.gitignore" "C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\README.md"
 
+:: 建立一個應該被排除的目錄列表
+set excludeDirs="C:\Users\weiti\Desktop\PARA\1_project\MC\file_public\.git"
 
-
+:: 使用 /XF 和 /XD 選項來排除文件和目錄
+robocopy "%source%" "%destination%" /MIR /mt /z /XF %excludeFiles% /XD %excludeDirs%
 
 exit /B
